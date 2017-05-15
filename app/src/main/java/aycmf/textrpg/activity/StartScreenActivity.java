c243b141a5b0bb731df4d0e3737834c00a877ff2
Michael <michael.yang.d@gmail.com>
2017-05-15 05:36:01 -0400

    Adding buttons

--- app/src/main/java/aycmf/textrpg/activity/StartScreenActivity.java
+++ app/src/main/java/aycmf/textrpg/activity/StartScreenActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import aycmf.textrpg.R;
@@ -14,6 +16,10 @@ import aycmf.textrpg.model.ModelContainer;
 public class StartScreenActivity extends AppCompatActivity {
     private boolean hasExistingGame = false;
     private ModelContainer modelContainer;
+    private Button startscreen_continue;
+    private Button startscreen_newgame;
+    private Button startscreen_achivements;
+    private Button startscreen_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,48 @@ public class StartScreenActivity extends AppCompatActivity {
         final Resources res = getResources();
         setContentView(R.layout.activity_start_screen);
         modelContainer = ModelContainer.load(this);
+
+        startscreen_continue = (Button) findViewById(R.id.startscreen_continue);
+        startscreen_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueGame();
+            }
+        });
+
+        if (modelContainer == null) {
+            startscreen_continue.setEnabled(false);
+        }
+
+        startscreen_newgame = (Button) findViewById(R.id.startscreen_newgame);
+        startscreen_newgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AppCompatEditText characterName = new AppCompatEditText(StartScreenActivity.this);
+                new AlertDialog.Builder(StartScreenActivity.this)
+                        .setTitle("Test")
+                        .setMessage(R.string.startscreen_entercharactername)
+                        .setView(characterName)
+                        .setIcon(android.R.drawable.ic_input_add)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                modelContainer = new ModelContainer();
+                                modelContainer.initializeGame(characterName.getText().toString());
+                                modelContainer.save(StartScreenActivity.this);
+                                //continueGame(true, characterName.getText().toString());
+                                //setButtonState(null, null);
+                            }
+                        })
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .create().show();
+            }
+        });
+
+        startscreen_achivements = (Button) findViewById(R.id.startscreen_achievements);
+        startscreen_settings = (Button) findViewById(R.id.startscreen_settings);
+
+        /*
         if (modelContainer == null) {
             final AppCompatEditText characterName = new AppCompatEditText(this);
             new AlertDialog.Builder(this)
@@ -43,6 +91,9 @@ public class StartScreenActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Successfully Loaded for "+ modelContainer.getCharacter().getName(), Toast.LENGTH_SHORT).show();
         }
+        */
     }
+    public void continueGame(){
 
+    }
 }
